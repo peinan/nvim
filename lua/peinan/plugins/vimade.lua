e40c870f93acf29d9ed782e909853f95ba8d12b7
Peinan Zhang <peinan7@gmail.com>
2026-01-26 16:55:15 +0900

    feat(vimade): add blocklist for floats and UI buffers
    - Exclude floating windows from vimade fading
    - Block fading for specific filetypes (e.g., telescope, notify, lazy)
    - Block fading for common non-editing buftypes (prompt, terminal, quickfix)

--- lua/peinan/plugins/vimade.lua
+++ lua/peinan/plugins/vimade.lua
@@ -1,4 +1,47 @@
 return {
     recipe = { "paradox", { animate = true } },
     fadelevel = 0.6,
+
+    blocklist = {
+        -- Block all floating windows (notifications, popups, etc.)
+        block_floats = function(win, active)
+            return win.win_config.relative ~= ""
+        end,
+
+        -- Block specific filetypes
+        filetypes = {
+            buf_opts = {
+                filetype = {
+                    "minifiles",
+                    "minifiles-help",
+                    "dashboard",
+                    "noice",
+                    "notify",
+                    "lazy",
+                    "TelescopePrompt",
+                    "TelescopeResults",
+                    "qf",
+                    "help",
+                    "man",
+                    "checkhealth",
+                    "DiffviewFiles",
+                    "no-neck-pain",
+                    "netrw",
+                },
+            },
+        },
+
+        -- Block specific buftypes
+        buftypes = {
+            buf_opts = {
+                buftype = {
+                    "nofile",
+                    "prompt",
+                    "help",
+                    "terminal",
+                    "quickfix",
+                },
+            },
+        },
+    },
 }
