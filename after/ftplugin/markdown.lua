8d5b92c942f2272c6cbbbe269e0a7b19f50b4b76
peinan <peinan7@gmail.com>
2026-06-16 14:49:58 +0900

    fix(markdown): disable spell check for markdown files

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,5 +1,6 @@
+-- spell は無効化する。spelllang=en だと辞書に無い日本語が全て SpellBad 判定になり、
+-- SpellBad の undercurl が undercurl 非対応の端末で下線にフォールバックして表示されるため。
 vim.cmd([[
     setlocal wrap
-    setlocal spell
-    setlocal spelllang=en
+    setlocal nospell
 ]])
